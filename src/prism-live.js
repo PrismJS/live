f82d0c7a14f3e4dd93ab93bb57ed81a1f2cb86d5
Lea Verou <lea@verou.me>
2024-02-13 13:59:38 -0500

    Fix CORS issue

--- src/prism-live.js
+++ src/prism-live.js
@@ -1,2 +1,8 @@
 
-import("./prism-live.mjs").then(m => Prism.Live = m.default);
+{
+	let url = document.currentScript?.src;
+	url = new URL(url, location);
+	let importURL = new URL("./prism-live.mjs", url);
+	importURL.search = url.search;
+	import(importURL).then(m => Prism.Live = m.default);
+}
